5f6f66b2e231cfcab2143e71ca530e6a714fd797
Rafay Hasan <rafayhasan@Rafays-MacBook-Pro-2.local>
2018-04-30 14:32:44 +0600

    Initial Commit

---
+++ Nicola/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Nicola
+//
+//  Created by Rafay Hasan on 4/30/18.
+//  Copyright © 2018 Rafay Hasan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
---
+++ Nicola/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Nicola
+//
+//  Created by Rafay Hasan on 4/30/18.
+//  Copyright © 2018 Rafay Hasan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
