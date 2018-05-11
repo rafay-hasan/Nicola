a765bb13a76f265efb0b02e6c6ee2fce51a4eca8
Rafay Hasan <Rafay@rafays-mac-mini.local>
2018-05-11 12:28:24 +0600

    Added menu table view with animation in home page.

--- Nicola/Home/HomeViewController.m
+++ Nicola/Home/HomeViewController.m
@@ -8,7 +8,12 @@
 
 #import "HomeViewController.h"
 
-@interface HomeViewController ()
+@interface HomeViewController ()<UITableViewDataSource,UITableViewDelegate>
+
+@property (strong,nonatomic) NSArray *menuArray;
+@property (weak, nonatomic) IBOutlet UITableView *menuTableview;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *menuTableheight;
+- (IBAction)menuButtonAction:(id)sender;
 
 @end
 
@@ -17,6 +22,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.menuArray = [[NSArray alloc]initWithObjects:@"la storia del club",@"li ultime notizie",@"li prossime gare ed eventi;",@"la hall of fame",@"Iscriviti al club",@"chat",@"profilo", nil];
+    self.menuTableview.tableFooterView = [[UIView alloc] initWithFrame:CGRectZero];
+
 }
 
 - (void)didReceiveMemoryWarning {
@@ -34,4 +42,67 @@
 }
 */
 
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.menuArray.count;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"menuCell" forIndexPath:indexPath];
+    cell.textLabel.text = [self.menuArray objectAtIndex:indexPath.row];
+    cell.textLabel.textColor = [UIColor colorWithRed:250.0/255.0 green:228.0/255.0 blue:3.0/255.0 alpha:1];
+    cell.backgroundColor = [UIColor clearColor];
+    return cell;
+}
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+- (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section {
+    
+    UIView *headerView = [[UIView alloc] init];
+    headerView.backgroundColor = [UIColor clearColor];//[UIColor colorWithRed:250.0/255.0 green:228.0/255.0 blue:3.0/255.0 alpha:1];
+    return headerView;
+}
+
+- (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section
+{
+    UIView *footerView = [[UIView alloc] init];
+    footerView.backgroundColor = [UIColor clearColor];//[UIColor colorWithRed:250.0/255.0 green:228.0/255.0 blue:3.0/255.0 alpha:1];
+    return footerView;
+}
+
+- (IBAction)menuButtonAction:(id)sender {
+    
+    if (self.menuTableheight.constant == 0) {
+//        [UIView animateWithDuration:0.9f animations:^{
+//            self.menuTableheight.constant = self.menuTableview.contentSize.height;
+//            [self.view setNeedsLayout];
+//        }];
+        [UIView transitionWithView:self.menuTableview
+                          duration:0.4
+                           options:UIViewAnimationOptionTransitionCrossDissolve
+                        animations:^{
+                            self.menuTableheight.constant = self.menuTableview.contentSize.height;
+                        }
+                        completion:NULL];
+    }
+    else {
+//        [UIView animateWithDuration:0.9f animations:^{
+//            self.menuTableheight.constant = 0;
+//            [self.view setNeedsLayout];
+//        }];
+        [UIView transitionWithView:self.menuTableview
+                          duration:0.4
+                           options:UIViewAnimationOptionTransitionCrossDissolve
+                        animations:^{
+                            self.menuTableheight.constant = 0;
+                        }
+                        completion:NULL];
+    }
+    
+}
 @end
